b1c174025f729549c4c39de90073a85c161aea7a
Kenneth Lee <kennethlee@fortinet.com>
2020-05-20 19:52:41 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# nega-toast
+# \<nega-toast\>
+
+Simple toast web component
+
+[![Published on webcomponents.org](https://img.shields.io/badge/webcomponents.org-published-blue.svg)](https://www.webcomponents.org/element/kennethklee/nega-toast)
+
+See: [Documentation](https://www.webcomponents.org/element/nega-toast),
+  [Demo](https://kennethklee.github.io/nega-toast/demo/).
+
+
+# Usage
+
+## Installation
+
+```shell
+npm install --save nega-toast
+```
+
+## In an html file
+
+```html
+<html>
+  <head>
+    <script type="module">
+      import 'nega-toast/nega-toast.js';
+    </script>
+  </head>
+  <body>
+    <nega-toast>Hi!</nega-toast>
+  </body>
+</html>
+```
+
+
+# Contributing
+
+Feel free to fork and send over PRs. Still a lot of places this can be improved, i.e. styling, more options, or better behaviors.
+
+## Installation
+
+```
+git clone https://github.com/kennethklee/nega-toast
+cd nega-toast
+npm install
+```
+
+## Running locally
+
+```
+$ npm start
+```
+
+## Running tests
+
+```
+$ npm test
+```
---
+++ docs/nega-toast.js
@@ -0,0 +1,91 @@
+/**
+Simple toast web component
+
+Example:
+
+```
+Basic: <nega-toast>Hi</nega-toast>
+```
+
+@element nega-toast
+@demo demo/index.html
+*/
+class NegaDonut extends HTMLElement {
+  constructor() {
+    super();
+    const template = Object.assign(document.createElement('template'), {
+      innerHTML: `
+      <style>
+        :host {
+          display: block;
+          position: fixed;
+
+          box-sizing: border-box;
+          min-width: 100px;
+          padding: 16px 24px;
+          
+          color: #dddddd;
+          border-radius: 2px;
+          background-color: #292929;
+
+          transition: transform 0.3s, opacity 0.3s;
+          transform: translateY(60px);
+          opacity: 0;
+        }
+
+        :host([opened]) {
+          transform: translateY(0);
+          opacity: 1;
+        }
+      </style>
+
+      <slot></slot>
+    `
+    });
+    this.attachShadow({
+      mode: 'open'
+    });
+    this.shadowRoot.appendChild(document.importNode(template.content, true));
+  }
+
+  static get observedAttributes() {
+    return ['opened', 'duration'];
+  }
+
+  get opened() {
+    return this.hasAttribute('opened');
+  }
+
+  set opened(value) {
+    value ? this.setAttribute('opened', '') : this.removeAttribute('opened');
+  }
+
+  get duration() {
+    return parseInt(this.getAttribute('duration'), 10);
+  }
+
+  set duration(value) {
+    if (!isNaN(value)) {
+      value ? this.setAttribute('duration', value) : this.removeAttribute('duration');
+    }
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    name === 'opened' && this.opened && this.duration && setTimeout(() => this.close(), this.duration);
+  }
+
+  open() {
+    this.opened = true;
+  }
+
+  close() {
+    this.opened = false;
+  }
+
+  toggle() {
+    this.opened = !this.opened;
+  }
+
+}
+
+window.customElements.define('nega-toast', NegaDonut);
